c3b97f9c8f73b0b5499cb0f2f8ab8bcbf55761ca
ShuVeriDA <bashtarov@outlook.com>
2022-07-26 18:26:40 +0300

    authreducer refactored to redux-toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
-import { combineReducers} from 'redux';
+import {AnyAction, combineReducers} from 'redux';
 import {tasksReducer} from "../features/TodolistsList/tasks-reducers";
 import {todolistsReducer} from "../features/TodolistsList/todolists-reducer";
-import {TypedUseSelectorHook, useSelector} from "react-redux";
-import thunkMiddleware from 'redux-thunk'
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import thunkMiddleware, {ThunkDispatch} from 'redux-thunk'
 import {appReducer} from "./app-reducer";
 import {authReducer} from "../features/Login/authReducer";
 import {configureStore} from "@reduxjs/toolkit";
@@ -24,6 +24,9 @@ export const store = configureStore({
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+export type DispatchType  = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+export const useAppDispatch = () => useDispatch<DispatchType>()
+
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
