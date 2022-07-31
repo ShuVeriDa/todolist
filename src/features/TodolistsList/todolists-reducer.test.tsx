80a549ab87af136adc464b8119dbb88704b3e6b0
ShuVeriDA <bashtarov@outlook.com>
2022-07-31 14:39:09 +0300

    changed test - 'correct todolist should change its name.'

--- src/features/TodolistsList/todolists-reducer.test.tsx
+++ src/features/TodolistsList/todolists-reducer.test.tsx
@@ -1,6 +1,6 @@
 import {v1} from "uuid";
 import {
-   addTodolistAC,
+   addTodolistAC, changeTodolistTitleAC,
    removeTodolistAC,
    TodolistDomainType,
    todolistsReducer
@@ -44,16 +44,17 @@ test('correct todolist should be added', () => {
    expect(endState[2].title).toBe(todolist.title)
    expect(endState[0].filter).toBe('all')
 })
-//
-// test('correct todolist should change its name.', () => {
-//
-//    const newTodolistTitle = "new Todolist"
-//
-//    const endState = todolistsReducer(startState, changeTodoListTitle(todolistID2, newTodolistTitle))
-//
-//    expect(endState[0].title).toBe("What to learn")
-//    expect(endState[1].title).toBe(newTodolistTitle)
-// })
+
+test('correct todolist should change its name.', () => {
+
+   const newTodolistTitle = "new Todolist"
+   const action = changeTodolistTitleAC({todolistId: todolistID2, title: newTodolistTitle})
+
+   const endState = todolistsReducer(startState, action)
+
+   expect(endState[0].title).toBe("What did you learn?")
+   expect(endState[1].title).toBe(newTodolistTitle)
+})
 //
 // test('correct filter of todolist should be changed', () => {
 //
